fc4a35d27d6689ac7b9f58deb6953dad3e43683e
Michael Stewart <michael.stewart@uci.edu>
2013-04-22 14:44:13 -0700

    initial copy from DokuWiki plugin page, with updates for 2013 urls

---
+++ conf/default.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Default settings for the googledrawing plugin
+ *
+ * @author Linus Brimstedt <linus@brimstedt.se>
+ */
+
+$conf['googleUrl']    = 'https://docs.google.com/';
---
+++ conf/metadata.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Options for the googledrawing plugin
+ *
+ * @author Linus Brimstedt <linus@brimstedt.se>
+ */
+
+
+$meta['googleUrl'] = array('string');
+
